29099a29d8ae64468f9506dd12ff84e2f0d7c3a6
Maria Alice <malice.rfort@gmail.com>
2021-11-10 09:21:10 -0300

    apaguei os arquivos de informacoes pois estavam dando erro, inclui os arquivos infos

---
+++ app/src/main/java/com/mariafortunato/calculadoraimc/InfosActivity.kt
@@ -0,0 +1,11 @@
+package com.mariafortunato.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_infos)
+    }
+}
